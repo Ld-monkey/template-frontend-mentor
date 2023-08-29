1ca0c86b1bdc9090408ff6eb19a47c85ee3727f3
Ld-monkey <lfourteau91@gmail.com>
2023-08-29 16:26:18 +0200

    try to config eslint with prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+      },
+    ],
+    'react/jsx-props-no-spreading': 0,
+    'jsx-a11y/label-has-associated-control': [2, { assert: 'either' }],
+    'linebreak-style': 0,
+    'no-restricted-imports': 0,
+  },
+};
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 function App() {
   return (
     <div>
+        <a href="#"><li><div></div></li></a>
       <h1>Hello world</h1>
     </div>
   );
